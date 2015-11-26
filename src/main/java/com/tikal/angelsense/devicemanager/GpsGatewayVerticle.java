ecea486c62949480221d2c71034bb8a8511fa6f0
yanai <yanai@tikalk.com>
2015-11-26 11:13:34 +0200

    dm

--- src/main/java/com/tikal/angelsense/devicemanager/GpsGatewayVerticle.java
+++ src/main/java/com/tikal/angelsense/devicemanager/GpsGatewayVerticle.java
@@ -1,6 +1,9 @@
 package com.tikal.angelsense.devicemanager;
 
+import com.cyngn.kafka.MessageProducer;
+
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.http.HttpServerRequest;
 
 public class GpsGatewayVerticle extends AbstractVerticle {
@@ -9,13 +12,15 @@ public class GpsGatewayVerticle extends AbstractVerticle {
 
 	@Override
 	public void start() {
+		vertx.deployVerticle(MessageProducer.class.getName(),new DeploymentOptions().setConfig(config()));
 		vertx.createHttpServer().requestHandler(req -> req.bodyHandler(b -> handleGps(req, b.toString()))).listen(config().getInteger("http-port"));
 		logger.info("Started HTTP Server on port {} to listen for GPS", config().getInteger("http-port"));
 	}
 
 	private void handleGps(final HttpServerRequest req, final String gpsPayload) {
 		logger.debug("Sending the GPS Reading:{}", gpsPayload);
-		vertx.eventBus().send("gps.all", gpsPayload);
+//		vertx.eventBus().send("gps.all", gpsPayload);
+		vertx.eventBus().send(MessageProducer.EVENTBUS_DEFAULT_ADDRESS, gpsPayload);
 		req.response().end();
 		// vertx.eventBus().send("gps.enrichment", gpsPayload, (final
 		// AsyncResult<Message<JsonObject>> ar) ->
